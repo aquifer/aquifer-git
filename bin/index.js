a2967c2418d3e9d17ae64f3b911e8f0d311005f8
Peter Sieg <chasingmaxwell@gmail.com>
2015-08-21 01:07:34 -0400

    Make everything work.

--- bin/index.js
+++ bin/index.js
@@ -10,12 +10,9 @@ module.exports = function(Aquifer, AquiferGitConfig) {
   'use strict';
 
   var AquiferGit  = function() {},
-      Q           = require('q'),
-      spawn       = require('child_process').spawn,
+      _           = require('lodash'),
       git         = require('nodegit'),
       mktemp      = require('mktemp'),
-      del         = require('del'),
-      fs          = require('fs-extra'),
       path        = require('path'),
       jsonFile    = require('jsonfile');
 
@@ -28,11 +25,20 @@ module.exports = function(Aquifer, AquiferGitConfig) {
   AquiferGit.commands = function () {
     return {
       'deploy-git': {
-        description: 'Deploys Drupal site to a Git repository.',
+        description: 'Deploys the site to a remote Git repository.',
         options: {
           remote: {
-            name: 'remote',
-            description: 'The Git remote to deploy to.',
+            name: '-r, --remote <remote>',
+            description: 'The repository to deploy to.'
+          },
+          branch: {
+            name: '-b, --branch <branch>',
+            description: 'The branch to deploy to.'
+          },
+          message: {
+            name: '-m, --message <message>',
+            default: 'Deployment from source repository.',
+            description: 'The message to use with the deployment commit.'
           }
         }
       }
@@ -43,121 +49,163 @@ module.exports = function(Aquifer, AquiferGitConfig) {
    * Run when user runs commands within this extension.
    *
    * @param string command string representing the name of the command defined in AquiferGit.commands that should run.
-   * @param function callback function that is called when there is a message to send.
+   * @param object commandOptions options passed from the command.
+   * @param function callback function that is called when there is an error message to send.
    */
-  AquiferGit.run = function (command, callback) {
+  AquiferGit.run = function (command, commandOptions, callback) {
     if (command !== 'deploy-git') {
-      callback('Invalid command');
+      callback('Invalid command.');
       return;
     }
 
-    var jsonPath  = path.join(Aquifer.projectDir, 'aquifer.json'),
-        json      = jsonFile.readFileSync(jsonPath),
-        srcPath   = path.join(Aquifer.projectDir, json.paths.builds, 'work'),
-        destPath, repo;
+    var jsonPath        = path.join(Aquifer.projectDir, 'aquifer.json'),
+        json            = jsonFile.readFileSync(jsonPath),
+        srcPath         = path.join(Aquifer.projectDir, json.paths.builds, 'work'),
+        make            = path.join(Aquifer.projectDir, json.paths.make),
+        requiredOptions = ['remote', 'branch', 'message'],
+        optionsMissing  = false,
+        options         = {},
+        build, destPath, repo, index;
 
-    // Build the site.
-    Q.Promise(function (resolve, reject, notify) {
-      console.log('building the site.');
-      spawn('aquifer', ['build'])
-        .on('close', function (code) {
-          if (code !== 0) {
-            reject('Something went wrong with the build. Error code: ' + code);
-          }
-          resolve();
-        });
-    })
-      // Make temporary directory.
-      .then(function () {
-        console.log('creating temp directory.');
-        return mktemp.createDir('tmp/XXXXXXX');
-      })
+    // Parse options and make sure they all exist.
+    _.assign(options, AquiferGitConfig, commandOptions, function (lastValue, nextValue, name) {
+      return nextValue ? nextValue : lastValue;
+    });
+    requiredOptions.forEach(function (name) {
+      if (!options[name]) {
+        callback('"' + name + '" option is missing. Cannot deploy.');
+        optionsMissing = true;
+      }
+    });
+    if (optionsMissing) {
+      return;
+    }
+
+    // Create the destination directory and initiate the promise chain.
+    mktemp.createDir('tmp/XXXXXXX')
+
       // Clone the repository.
-      .then(function (res) {
-        console.log('cloning the repository.');
-        destPath = res;
-        console.log(destPath);
-        return git.Clone.clone(AquiferGitConfig.repository, destPath, {
+      .then(function (destPath_) {
+        Aquifer.console.log('Cloning the repository into ' + destPath_ + '.', 'status');
+
+        destPath = destPath_;
+
+        var cloneOptions = {
           remoteCallbacks: {
             certificateCheck: function() { return 1; },
             credentials: function(url, userName) {
               return git.Cred.sshKeyFromAgent(userName);
             }
           }
-        });
+        };
+
+        return git.Clone.clone(options.remote, destPath, cloneOptions);
       })
-      // Checkout or create the deployment branch.
-      .then(function (res) {
-        console.log('checkout the deployment branch.');
-        repo = res;
-        return repo.checkoutBranch(AquiferGitConfig.branch)
-          .then(function (branch) {
-            console.log('branch exists.');
-            // The branch exists.
+
+      // Checkout the deployment branch.
+      .then(function (repo_) {
+        repo = repo_;
+        return repo.checkoutBranch(options.branch)
+          .then(function () {
+            Aquifer.console.log('Checking out the ' + options.branch + ' branch...', 'status');
             return;
           })
           .catch(function (err) {
-            console.log('branch does not exist.');
-            // Create the branch if it didn't exist.
-            return repo.createBranch(AquiferGitConfig.branch, repo.getHeadCommit())
-              .then(function (branch) {
-                return;
+            console.log(err);
+            Aquifer.console.log('Creating the ' + options.branch + ' branch...', 'status');
+
+            // Get the current HEAD commit.
+            return repo.getHeadCommit()
+              .then(function(commit) {
+                // Create the new branch at the current HEAD.
+                return repo.createBranch(options.branch, commit);
+              })
+              .then(function () {
+                // Checkout the newly created branch.
+                return repo.checkoutBranch(options.branch);
+              })
+              .catch(function (err) {
+                // Make sure we escelate this error up the chain.
+                throw err;
               });
           });
       })
-      // Remove everything except the .git directory.
+
+      // Build the site.
       .then(function () {
+        Aquifer.console.log('Building the site...', 'status');
+
+        var buildOptions = {
+          symlink: false,
+          delPatters: ['*', '!.git']
+        };
+
+        build = new Aquifer.api.build(destPath, buildOptions);
+
         return new Promise(function (resolve, reject) {
-          console.log('remove everything.');
-          del([destPath + '/*', '!' + destPath + '/.git'], function (err, paths) {
-            if (err) {
-              reject(err);
+          build.create(make, function (error) {
+            if (error) {
+              reject();
             }
-            resolve(paths);
+            else {
+              resolve();
+            }
           });
         });
       })
-      // Copy the built site to the repository.
-      .then(function () {
-        return new Promise(function (resolve, reject) {
-          console.log('copy over the build.');
-          fs.copy(srcPath, destPath, function (err) {
-            if (err) {
-              reject(err);
-            }
-            resolve();
-          });
-        });
-      })
-      // Add all files.
+
+      // Add all files to the index.
       .then(function() {
-        console.log('add all files.');
-        return repo.index()
-          .then(function(index) {
-            return index.removeAll()
-              .then(function() {
-                index.write();
-              })
-              .then(function() {
-                return index.addAll();
-              })
-              .then(function() {
-                index.write();
-              });
-          });
+        Aquifer.console.log('Adding all files to the index...', 'status');
+        return repo.index();
       })
+      .then(function(index_) {
+        index = index_;
+        return index.removeAll();
+      })
+      .then(function() {
+        index.write();
+      })
+      .then(function() {
+        return index.addAll();
+      })
+      .then(function() {
+        index.write();
+      })
+
       // Commit changes.
       .then(function () {
-        console.log('commit changes.');
+        Aquifer.console.log('Commit changes...', 'status');
         var signature = git.Signature['default'](repo);
 
-        return repo.createCommitOnHead(['.'], signature, signature, 'Automated deployment.');
+        return repo.createCommitOnHead(['.'], signature, signature, options.message);
       })
+
       // Push changes.
       .then(function () {
-        // @TODO: actually push changes.
-        console.log('it worked.');
+        Aquifer.console.log('Pushing changes to origin/' + options.branch + '...', 'status');
+        return repo.getRemote('origin');
       })
+      .then(function (remote) {
+        var ref   = 'refs/heads/' + options.branch,
+            refs  = [ref + ':' + ref];
+
+        remote.setCallbacks({
+          certificateCheck: function() { return 1; },
+          credentials: function(url, userName) {
+            return git.Cred.sshKeyFromAgent(userName);
+          }
+        });
+
+        return remote.push(refs);
+      })
+
+      // Success!
+      .then(function () {
+        Aquifer.console.log('The site has been successfully deployed!', 'success');
+      })
+
+      // Catch any errors.
       .catch(function (err) {
         callback(err);
       });
